b0e56becf91af17448334c81a5c5f6de34a0de18
rahulranjansah <rahulranjann45@gmail.com>
2024-03-24 02:07:28 -0400

    dtype builder

--- clean.py
+++ clean.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import numpy as np
 
-def prep_ufo_data(df):
+def raw_data_cleaner(df):
+
+    """
+    Function for building datetime series, numeric series, string series for respective features
+    """
 
     def custom_to_datetime(date):
         if date[11:13] == "24":
@@ -15,10 +19,27 @@ def prep_ufo_data(df):
 
         return pd.to_datetime(x)
 
-# numeric_val and cleaning position
-df["latitude"] = pd.to_numeric(df["latitude"], errors="coerce")
-df["lognitude"] = pd.to_numeric(df["longitude"], errors="coerce")
+    # setting numeric_val and cleaning position dtypes
+    df["latitude"] = pd.to_numeric(df["latitude"], errors="coerce")
+    df["lognitude"] = pd.to_numeric(df["longitude"], errors="coerce")
+    df["duration (seconds)"] = pd.to_numeric(df["duration (seconds)"], errors="coerce")
 
-df.loc[:,"latitude"].replace({0.0: np.nan}, inplace=True)
-df.loc[:, "longitude"].replace({0.0: np.nan}, inplace=True)
-df.index = np
+    df.loc[:,"latitude"].replace({0.0: np.nan}, inplace=True)
+    df.loc[:, "longitude"].replace({0.0: np.nan}, inplace=True)
+    df.loc[:,"duration (seconds)"].replace({0.0: np.nan}, inplace=True)
+    df.index = np.arange(0, len(df))
+
+    # string series dtype for comments
+    df["comments"] = df["comments"].astype(str)
+
+    # initialzing datetime series with time of report
+    df["datetime"] = df["datetime"].apply(custom_to_datetime)
+    df["date posted"] = pd.to_datetime(df["date posted"])
+
+    df["month"] = pd.DatetimeIndex(df["datetime"]).month
+    df["year"] = pd.DatetimeIndex(df["datetime"]).year
+    df["day"] = pd.DatetimeIndex(df["datetime"]).day
+    df["hour"] = pd.DatetimeIndex(df["datetime"]).hour
+
+    return df
+
